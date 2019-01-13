c6682708c10a93cea645ea775908c5ed18e3c275
Henry <henry@henrymoulton.com>
2019-01-13 19:20:34 +0000

    Remove Mobx and the linked Navigation

--- boilerplate/src/app/root-component.tsx
+++ boilerplate/src/app/root-component.tsx
@@ -1,44 +1,18 @@
 import "../i18n"
 import * as React from "react"
-import { setupRootStore } from "./setup-root-store"
-import { StatefulNavigator } from "../navigation"
-import { RootStore } from "./root-store"
-import { Provider } from "mobx-react"
-import { BackButtonHandler } from "../navigation/back-button-handler"
-import { contains } from "ramda"
-import { DEFAULT_NAVIGATION_CONFIG } from "../navigation/navigation-config"
-
-interface RootComponentState {
-  rootStore?: RootStore
-}
+import { RootNavigator } from "../navigation"
 
 /**
  * This is the root component of our app.
  */
-export class RootComponent extends React.Component<{}, RootComponentState> {
+export class RootComponent extends React.Component {
   /**
    * When the component is mounted. This happens asynchronously and simply
    * re-renders when we're good to go.
    */
-  async componentDidMount() {
-    this.setState({
-      rootStore: await setupRootStore(),
-    })
-  }
-
-  /**
-   * Are we allowed to exit the app?  This is called when the back button
-   * is pressed on android.
-   *
-   * @param routeName The currently active route name.
-   */
-  canExit(routeName: string) {
-    return contains(routeName, DEFAULT_NAVIGATION_CONFIG.exitRoutes)
-  }
+  async componentDidMount() {}
 
   render() {
-    const rootStore = this.state && this.state.rootStore
-
     // Before we show the app, we have to wait for our state to be ready.
     // In the meantime, don't render anything. This will be the background
     // color set in native by rootView's background color.
@@ -47,22 +21,14 @@ export class RootComponent extends React.Component<{}, RootComponentState> {
     //
     // You're welcome to swap in your own component to render if your boot up
     // sequence is too slow though.
-    if (!rootStore) {
-      return null
-    }
+
+    // TODO: Commented by Henry - to add Redux or GraphQL?
+    // if (!rootStore) {
+    //   return null
+    // }
 
     // otherwise, we're ready to render the app
 
-    // --- am: begin list of stores ---
-    const otherStores = {}
-    // --- am: end list of stores ---
-
-    return (
-      <Provider rootStore={rootStore} navigationStore={rootStore.navigationStore} {...otherStores}>
-        <BackButtonHandler canExit={this.canExit}>
-          <StatefulNavigator />
-        </BackButtonHandler>
-      </Provider>
-    )
+    return <RootNavigator />
   }
 }
--- boilerplate/src/navigation/root-navigator.ts
+++ boilerplate/src/navigation/root-navigator.ts
@@ -1,12 +1,14 @@
-import { createStackNavigator } from "react-navigation"
+import { createAppContainer, createStackNavigator } from "react-navigation"
 import { ExampleNavigator } from "./example-navigator"
 
-export const RootNavigator = createStackNavigator(
-  {
-    exampleStack: { screen: ExampleNavigator },
-  },
-  {
-    headerMode: "none",
-    navigationOptions: { gesturesEnabled: false },
-  },
+export const RootNavigator = createAppContainer(
+  createStackNavigator(
+    {
+      exampleStack: { screen: ExampleNavigator },
+    },
+    {
+      headerMode: "none",
+      navigationOptions: { gesturesEnabled: false },
+    },
+  ),
 )
